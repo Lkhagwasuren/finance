54ffadc45b0d5b155da890b056018fa8ba59f48f
unknown <lkhagwal41@gmail.com>
2026-07-03 19:01:48 +0800

    garaas ogogdol awah dom-uudiig  olloo

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
 // Delgetstei ajillah controller
-var uiController  = (function () {
-    
+var uiController = (function () {
+  // DOM-oos awsan  ogogdolvvdiig end hadgaliy
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
 })();
 // sanhuutei  ajillah controller
 
-var financeController  = (function () {
-    
-})();
+var financeController = (function () {})();
 
 //programmiin holbogch controller
-var appController  = (function (uiController, financeController) {
-
-    var ctrlAddItem = function () {
-        //1. oruulah ogogdoliiig delgetsees olj awna
-        console.log('delgetsees ogogdol awah');
-        //2. olj awsan ogogdliig sanhuutei ajillah controller ruu damjuulna
-        //3. olj awsan ogogdliig delgetsend ni gargana
-        //4. tolowiig tootsoolno
-        //5. etsesiin vldegdel tootsoog delgetsend gargana
-    };
-    document.querySelector('.add__btn').addEventListener('click', function () {      
-        ctrlAddItem();
-    });
-    document.addEventListener('keypress', function (event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
-        }
-    });
-}(uiController, financeController));
+var appController = (function (uiController, financeController) {
+  var DOM = uiController.getDOMstrings();
+  var ctrlAddItem = function () {
+    //1. oruulah ogogdoliiig delgetsees olj awna
+    console.log(uiController.getInput());
+    //2. olj awsan ogogdliig sanhuutei ajillah controller luu damjuulna
+    //3. olj awsan ogogdliig delgetsend ni gargana
+    //4. tolowiig tootsoolno
+    //5. etsesiin vldegdel tootsoog delgetsend gargana
+  };
+  document.querySelector(DOM.addBtn).addEventListener("click", function () {
+    ctrlAddItem();
+  });
+  document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
+})(uiController, financeController);
